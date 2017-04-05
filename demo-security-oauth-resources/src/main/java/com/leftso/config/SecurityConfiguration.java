b1f000e77fc8bd606fa9d349a46a7261a3e4bdf4
leftso <leftso@qq.com>
2017-04-05 09:40:09 +0800

    modify

--- demo-security-oauth-resources/src/main/java/com/leftso/config/SecurityConfiguration.java
+++ demo-security-oauth-resources/src/main/java/com/leftso/config/SecurityConfiguration.java
@@ -1,11 +1,7 @@
 package com.leftso.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,15 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("app_client").password("nopass").roles("USER")
-                .and()
-                .withUser("admin").password("password").roles("ADMIN");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,12 +21,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().csrf().disable();
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 
 }
